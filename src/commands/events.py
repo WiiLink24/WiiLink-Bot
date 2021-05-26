1e2ab7e6e988b600fadbfc209c527f4d0e357d85
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-05-25 23:05:48 -0400

    Only log deletions/edits if it is a user, not bot

--- src/commands/events.py
+++ src/commands/events.py
@@ -15,21 +15,38 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
-        user = message.author
-        text = f"{timestamp} :x: **{user}** (ID: {user.id})'s message has been deleted from <#{message.channel.id}>:"
-        embed = discord.Embed(color=0xFF0000)
-        embed.add_field(name="Deleted Message:", value=message.content, inline=True)
-        channel = self.bot.get_channel(819217765188501536)
-        await channel.send(text, embed=embed)
+        if not message.author.bot:
+            user = message.author
+            text = f"{timestamp} :x: **{user}** (ID: {user.id})'s message has been deleted from <#{message.channel.id}>:"
+            embed = discord.Embed(color=0xFF0000)
+            embed.add_field(name="Deleted Message:", value=message.content, inline=True)
+            channel = self.bot.get_channel(755522585864962099)
+            await channel.send(text, embed=embed)
 
     @commands.Cog.listener()
     async def on_message_edit(self, message_before, message_after):
-        text = f"{timestamp} :warning: **{message_before.author}** (ID: {message_before.author.id}) edited a message in <#{message_before.channel.id}>:"
-        embed = discord.Embed(color=0xFFFF00)
-        embed.add_field(name="From:", value=message_before.content, inline=False)
-        embed.add_field(name="\nTo:", value=message_after.content, inline=False)
-        channel = self.bot.get_channel(819217765188501536)
-        await channel.send(text, embed=embed)
+        if not message_before.author.bot:
+            text = f"{timestamp} :warning: **{message_before.author}** (ID: {message_before.author.id}) edited a message " \
+                   f"in <#{message_before.channel.id}>: "
+            embed = discord.Embed(color=0xFFFF00)
+            embed.add_field(name="From:", value=message_before.content, inline=False)
+            embed.add_field(name="\nTo:", value=message_after.content, inline=False)
+            channel = self.bot.get_channel(755522585864962099)
+            await channel.send(text, embed=embed)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        text = f"{timestamp} :inbox_tray: **{member}** (ID:{member.id}) joined the server.\n" \
+               f"Creation: {member.created_at}"
+        channel = self.bot.get_channel(755522585864962099)
+        await channel.send(text)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member: discord.Member):
+        text = f"{timestamp} :outbox_tray: **{member}** (ID:{member.id}) has been kicked or left the server.\n" \
+               f"Creation: {member.created_at}"
+        channel = self.bot.get_channel(755522585864962099)
+        await channel.send(text)
 
 
 def setup(bot):

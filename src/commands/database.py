2e2723fdbe9151ba217ae70ac340958c5e7a7df6
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-06-01 13:56:47 -0400

    Import os

--- src/commands/database.py
+++ src/commands/database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import os
+
 SQL_ALCHEMY_URI = f"postgresql://{os.getenv('WL24_BOT_DB_USER')}:{os.getenv('WL24_BOT_PASSWORD')}@postgres/{os.getenv('WL24_BOT_DATABASE')}"
 
 engine = create_engine(

f34025e48fcf7fd0e3e94837feec643920f4fc2a
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-06-01 13:40:14 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,11 @@
 import sqlalchemy as db
-from src.commands.database import Base
+from src.commands.connect_db import Base
+
 
 class UserInfo(Base):
     __tablename__ = "userinfo"
 
     userid = db.Column(db.BigInteger, primary_key=True)
     strikes = db.Column(db.Integer)
+    is_afk = db.Column(db.Boolean)
+    afk_reason = db.Column(db.String)

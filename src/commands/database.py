c3b3722c02ac4305ab97b58180619a2c6b997c89
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-06-01 13:49:36 -0400

    Use .env for database

--- src/commands/database.py
+++ src/commands/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_ALCHEMY_URI = "postgresql://username:password@127.0.0.1/wl24bot"
+SQL_ALCHEMY_URI = f"postgresql://{os.getenv('WL24_BOT_DB_USER')}:{os.getenv('WL24_BOT_PASSWORD')}@postgres/{os.getenv('WL24_BOT_DATABASE')}"
 
 engine = create_engine(
     SQL_ALCHEMY_URI

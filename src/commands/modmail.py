8a137002a3fbd0a2d17bcc817ac5c35a444cff76
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-06-28 19:11:57 -0400

    Update Mod Mail

--- src/commands/modmail.py
+++ src/commands/modmail.py
@@ -12,12 +12,15 @@ class ModMail(commands.Cog):
         fmt = "%m/%d/%Y"
         if not message.author.bot:
             if str(message.channel.type) == "private":
+                member = self.bot.get_user(int(message.author.id))
+                create_dm = await member.create_dm()
                 modmail_channel = discord.utils.get(self.bot.get_all_channels(), name="mod-mail")
                 embed = discord.Embed(colour=0x00FF00)
                 embed.set_author(name=f"{message.author.display_name} #{message.author.discriminator} (ID: {message.author.id})", icon_url=message.author.avatar_url)
                 embed.add_field(name="Mail Received", value=message.content, inline=False)
                 embed.set_footer(text=f"Submitted • {message.created_at.strftime(fmt)}")
                 await modmail_channel.send(embed=embed)
+                await create_dm.send("Message sent to staff.")
 
     @commands.command()
     async def modreply(self, ctx, user_id, *, reply_msg):
@@ -27,6 +30,7 @@ class ModMail(commands.Cog):
         embed.set_author(name="Mod Mail", icon_url=str(ctx.guild.icon_url))
         embed.add_field(name="Staff Response", value=reply_msg, inline=False)
         await create_dm.send(embed=embed)
+        await ctx.send("Reply sent.")
 
 
 def setup(bot):

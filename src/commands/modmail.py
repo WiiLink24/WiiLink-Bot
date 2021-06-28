34551ee26d6055b2721581f80103d6aff8607877
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-06-28 18:18:45 -0400

    Add Mod Mail

---
+++ src/commands/modmail.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+
+import discord
+
+
+class ModMail(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        fmt = "%m/%d/%Y"
+        if not message.author.bot:
+            if str(message.channel.type) == "private":
+                modmail_channel = discord.utils.get(self.bot.get_all_channels(), name="mod-mail")
+                embed = discord.Embed(colour=0x00FF00)
+                embed.set_author(name=f"{message.author.display_name} #{message.author.discriminator} (ID: {message.author.id})", icon_url=message.author.avatar_url)
+                embed.add_field(name="Mail Received", value=message.content, inline=False)
+                embed.set_footer(text=f"Submitted • {message.created_at.strftime(fmt)}")
+                await modmail_channel.send(embed=embed)
+
+    @commands.command()
+    async def modreply(self, ctx, user_id, *, reply_msg):
+        member = self.bot.get_user(int(user_id))
+        create_dm = await member.create_dm()
+        embed = discord.Embed(colour=0x00FF00)
+        embed.set_author(name="Mod Mail", icon_url=str(ctx.guild.icon_url))
+        embed.add_field(name="Staff Response", value=reply_msg, inline=False)
+        await create_dm.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(ModMail(bot))
--- wiilink24.py
+++ wiilink24.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
+intents = discord.Intents().all()
 intents.members = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
@@ -16,6 +16,7 @@ bot.load_extension("src.commands.mod")
 bot.load_extension("src.commands.events")
 bot.load_extension("src.commands.converters")
 bot.load_extension("src.commands.gametdb")
+bot.load_extension("src.commands.modmail")
 bot.load_extension("src.commands.afk")
 
 bot.run(TOKEN)

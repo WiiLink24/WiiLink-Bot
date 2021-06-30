75277aea56ba7d02e83abb12ae6a849e3619c46a
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-06-30 15:38:34 -0400

    Update Mod Mail

--- src/commands/modmail.py
+++ src/commands/modmail.py
@@ -12,15 +12,18 @@ class ModMail(commands.Cog):
         fmt = "%m/%d/%Y"
         if not message.author.bot:
             if str(message.channel.type) == "private":
-                member = self.bot.get_user(int(message.author.id))
-                create_dm = await member.create_dm()
-                modmail_channel = discord.utils.get(self.bot.get_all_channels(), name="mod-mail")
-                embed = discord.Embed(colour=0x00FF00)
-                embed.set_author(name=f"{message.author.display_name} #{message.author.discriminator} (ID: {message.author.id})", icon_url=message.author.avatar_url)
-                embed.add_field(name="Mail Received", value=message.content, inline=False)
-                embed.set_footer(text=f"Submitted • {message.created_at.strftime(fmt)}")
-                await modmail_channel.send(embed=embed)
-                await create_dm.send("Message sent to staff.")
+                if message.author.id != "729135459405529118":
+                    member = self.bot.get_user(int(message.author.id))
+                    create_dm = await member.create_dm()
+                    modmail_channel = discord.utils.get(self.bot.get_all_channels(), name="mod-mail")
+                    embed = discord.Embed(colour=0x00FF00)
+                    embed.set_author(
+                        name=f"{message.author.display_name} #{message.author.discriminator} (ID: {message.author.id})",
+                        icon_url=message.author.avatar_url)
+                    embed.add_field(name="Mail Received", value=message.content, inline=False)
+                    embed.set_footer(text=f"Submitted • {message.created_at.strftime(fmt)}")
+                    await modmail_channel.send(embed=embed)
+                    await create_dm.send("Message sent to staff.")
 
     @commands.command()
     async def modreply(self, ctx, user_id, *, reply_msg):

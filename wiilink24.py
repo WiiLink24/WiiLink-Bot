d2c2a4e3f5f2443433d5b0486d8ff47b2998a921
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2021-08-26 18:20:20 -0400

    Update wiilink24.py

--- wiilink24.py
+++ wiilink24.py
@@ -3,6 +3,7 @@ import discord
 
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord_components import DiscordComponents
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -10,13 +11,14 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents().all()
 intents.members = True
 bot = commands.Bot(command_prefix='/', intents=intents)
+DiscordComponents(bot)
 
 bot.load_extension("src.commands.misc")
 bot.load_extension("src.commands.mod")
 bot.load_extension("src.commands.events")
 bot.load_extension("src.commands.converters")
-bot.load_extension("src.commands.gametdb")
 bot.load_extension("src.commands.modmail")
 bot.load_extension("src.commands.afk")
+bot.load_extension("src.commands.music")
 
 bot.run(TOKEN)

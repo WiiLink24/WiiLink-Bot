76726ef71c8002f98846b6b51c149906885e9319
Harry Walker <contact@harrywalker.uk>
2026-01-02 20:45:17 +0000

    update error command

--- src/main/java/com/wiilink24/bot/commands/misc/ErrorCodes.java
+++ src/main/java/com/wiilink24/bot/commands/misc/ErrorCodes.java
@@ -89,7 +89,7 @@ public class ErrorCodes {
 
             String title = "Here's information about your error:";
             String description = channelErrors.get(codeNum);
-            String footer = "All information provided by RC24 Developers.";
+            String footer = "All information provided by WiiLink Developers.";
 
             EmbedBuilder builder = new EmbedBuilder();
             builder.setTitle(title);
@@ -186,8 +186,8 @@ public class ErrorCodes {
                             }
 
                             // Check for dev note
-                            if (codeNotes.containsKey(json.error) && !codeNotes.containsKey(json.error))
-                                infoBuilder.append("Note from RiiConnect24: ").append(codeNotes.get(json.error));
+                            if (codeNotes.containsKey(json.error))
+                                infoBuilder.append("Note from WiiLink: ").append(codeNotes.get(json.error));
 
                             String title = "Here's information about your error:";
                             description = infoBuilder.toString();
@@ -228,45 +228,27 @@ public class ErrorCodes {
     }
 
     private static final Map<Integer, String> channelErrors = new HashMap<>() {{
-        put(1, "Can't open the VFF. Follow https://wii.guide/deleting-vffs to fix it.");
+        put(1, "Can't open the VFF.");
         put(2, "Seems to happen when there is a problem with one of the files on the NAND. " + "If you're getting it after fixing NEWS/FORE000006, do a connection test to fix it.");
-        put(3, "VFF file corrupted. Follow https://wii.guide/deleting-vffs to fix it.");
+        put(3, "VFF file corrupted.");
         put(4, "This error probably doesn't exist.");
         put(5, "Seems to happen when there is a problem with the VFF. If you're getting this on the Wii," + "follow https://wii.guide/deleting-vffs to fix it." + "If you're getting this on Dolphin, make sure you're using the VFF Downloader and make sure that it's working.");
-        put(6, "Invalid data. If you're getting this on the **Forecast Channel**, try again in a few minutes. " + "If you're still getting this error, make sure your Wii's time is set correctly and wait a while." + "If you're getting this on the **News Channel**, follow https://wii.guide/news000006");
-        put(99, "Other error. Follow https://wii.guide/deleting-vffs to potentially fix it.");
+        put(6, "Follow https://wii.hacks.guide/wiiconnect24#updating-rtc-clock and try again.");
+        put(99, "Other error.");
     }};
 
     private static final Map<Integer, String> codeNotes = new HashMap<>() {{
-        put(101409, "If you are getting this error while doing something with Wii Mail, check if you patched the nwc24msg.cfg correctly using the Mail-Patcher. https://bit.ly/2QUrsyD");
-        put(102032, "This error shouldn't happen anymore as we have implemented the challenge response that prevents the error from happening.");
-        put(102409, "If you are getting this error while doing something with Wii Mail, check if you patched the nwc24msg.cfg correctly using the Mail-Patcher. https://bit.ly/2QUrsyD");
-        put(103409, "If you are getting this error while doing something with Wii Mail, check if you patched the nwc24msg.cfg correctly using the Mail-Patcher. https://bit.ly/2QUrsyD");
-        put(104409, "If you are getting this error while doing something with Wii Mail, check if you patched the nwc24msg.cfg correctly using the Mail-Patcher. https://bit.ly/2QUrsyD");
-        put(105409, "If you are getting this error while doing something with Wii Mail, check if you patched the nwc24msg.cfg correctly using the Mail-Patcher. https://bit.ly/2QUrsyD");
-        put(107006, "If you are getting this on the News Channel, this error means that the total size of the news files on the server is more than the Wii can handle. If so, please tell Larsenv you're getting this error and tell him your country and language your Wii is set to.");
-        put(107245, "Your IOS probably aren't patched. Go to https://wii.guide/riiconnect24 for instructions on how to patch them.");
-        put(107304, "This error can be caused by your ISP blocking custom DNS servers. Turn on Auto-obtain DNS in the Wii Settings, or use https://github.com/RiiConnect24/DNS-Server/releases/latest");
-        put(107305, "Try again. If it still doesn't work, it might be a problem with your Internet or RiiConnect24's servers.");
-        put(110211, "If you're getting this, tell Larsenv or KcrPL your Wii Number and they will delete it from the database so you can re-register with the mail patcher.");
-        put(110220, "Looks like the password your Wii uses isn't matching the one on the server. If you're getting this, tell Larsenv or KcrPL your Wii Number and they will delete it from the database so you can reregister with the mail patcher.");
-        put(110230, "Looks like the password your Wii uses isn't matching the one on the server. If you're getting this, tell Larsenv or KcrPL your Wii Number and they will delete it from the database so you can reregister with the mail patcher.");
-        put(110240, "Looks like the password your Wii uses isn't matching the one on the server. If you're getting this, tell Larsenv or KcrPL your Wii Number and they will delete it from the database so you can reregister with the mail patcher.");
-        put(110250, "Looks like the password your Wii uses isn't matching the one on the server. If you're getting this, tell Larsenv or KcrPL your Wii Number and they will delete it from the database so you can reregister with the mail patcher.");
-        put(117400, "This is an HTTP 400 Bad Request error. If you're getting this, tell Larsenv where you're getting this error on.");
-        put(117403, "This is an HTTP 403 Forbidden error. If you're getting this, tell Larsenv where you're getting this error on.");
-        put(117404, "This is an HTTP 404 Not Found error. If you're getting this, tell Larsenv where you're getting this error on.");
-        put(117500, "This is an HTTP 500 Internal Server error. If you're getting this, tell Larsenv where you're getting this error on.");
-        put(117503, "This is an HTTP 503 Service Unavailable error. If you're getting this, tell Larsenv where you're getting this error on.");
-        put(20103, "Delete DWC_AUTHDATA file stored in nand:/shared2/ using WiiXplorer.");
+        put(107245, "Make sure you've installed the latest patched WAD for the relevant channel.");
+        put(107304, "Try again. If it still doesn't work, it might be a problem with your internet connection.");
+        put(107305, "Try again. If it still doesn't work, it might be a problem with your internet connection.");
+        put(117410, "If you are receiving this in the News Channel, follow https://wiilink.ca/guide/update to update it.");
         put(231000, "Restart the Channel or your Wii then try again.");
-        put(231401, "You are not using the patched WAD for the Everybody Votes Channel. Please follow this tutorial: https://wii.guide/riiconnect24");
-        put(231409, "You are not using the patched WAD for the Everybody Votes Channel. Please follow this tutorial: https://wii.guide/riiconnect24");
-        put(239001, "Your IOS probably aren't patched. Go to https://wii.guide/riiconnect24 for instructions on how to patch them. Occasionally, this error can mean it downloaded invalid data.");
-        put(258404, "This is a 404 Not Found error.");
-        put(268503, "This is a 503 Service Unavailable error. If you are getting this on Nintendo Channel, just ignore it and press OK.");
-        put(32007, "You can no longer do a system update on the Wii because the server hosting the update is no longer up. Use https://wii.guide/update instead.");
-        put(33020, "If you're getting this on the Check Mii Out Channel, please repatch the Channel using RiiConnect24 Patcher.");
+        put(231401, "You are not using the patched WAD for the Everybody Votes Channel. Please follow https://wiilink.ca/guide/install.");
+        put(231409, "You are not using the patched WAD for the Everybody Votes Channel. Please follow https://wiilink.ca/guide/install.");
+        put(239001, "This is a server-side issue that does not affect functionality, just press OK.");
+        put(258302, "Follow https://wiilink.ca/guide/update to update the Check Mii Out Channel.");
+        put(260400, "If you are receiving this in the Nintendo Channel, follow https://wiilink.ca/guide/update to update it.")
+        put(33020, "If you are receiving this in the Check Mii Out Channel, follow https://wiilink.ca/guide/update to update it.");
         put(51330, "Try changing your router's settings to use 802.11 b/g/n. If that doesn't work, try the suggestions found on Nintendo's site. https://bit.ly/2OoC0c2");
         put(51331, "Try changing your router's settings to use 802.11 b/g/n. If that doesn't work, try the suggestions found on Nintendo's site. https://bit.ly/2OoC0c2");
         put(51332, "Try changing your router's settings to use 802.11 b/g/n. If that doesn't work, try the suggestions found on Nintendo's site. https://bit.ly/2OoC0c2");
